e5530410b719701e684afb7c0c2bfa1ffd929833
Matt Brown <matt@openline.ai>
2024-08-03 16:24:20 +0100

    free email check

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/customeros/mailhawk/internal/mx"
-	smpt "github.com/customeros/mailhawk/internal/smtp"
+	"github.com/customeros/mailhawk/internal/checks"
 )
 
 func main() {
@@ -14,22 +13,6 @@ func main() {
 		return
 	}
 	email := os.Args[1]
-
-	mxRecord, _ := mx.GetMXRecordsForEmail(email)
-	fmt.Println(mxRecord)
-	fmt.Println(mx.GetEmailServiceProviderFromMX(mxRecord))
-	fmt.Println("Firewall:", mx.IsFirewall(mx.GetEmailServiceProviderFromMX(mxRecord)))
-	esp, _ := mx.GetEmailProvidersFromSPF(email)
-	fmt.Println("ESPs:", esp)
-
-	proxy := smpt.ProxySetup{
-		Enable:   false,
-		Address:  "212.116.243.131:12323",
-		Username: "14a8e1626bbd4",
-		Password: "95f42a5e9c",
-	}
-
-	verified, err := smpt.VerifyEmailAddress(email, "microsoft.com", "steve.balmer@microsoft.com", proxy)
-	fmt.Println("Verified:", verified)
-	fmt.Println(err)
+	freeList := "/Users/mbrown/src/github.com/customeros/mailhawk/free_emails.toml"
+	fmt.Println(checks.IsFreeEmailCheck(email, freeList))
 }
