5cd095e61baf23a6503146ed739c3cd1a2409286
Matt Brown <matt@openline.ai>
2024-08-03 15:19:54 +0100

    working smpt w/ proxy

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/customeros/mailhawk/internal/mx"
+	smpt "github.com/customeros/mailhawk/internal/smtp"
 )
 
 func main() {
@@ -20,4 +21,15 @@ func main() {
 	fmt.Println("Firewall:", mx.IsFirewall(mx.GetEmailServiceProviderFromMX(mxRecord)))
 	esp, _ := mx.GetEmailProvidersFromSPF(email)
 	fmt.Println("ESPs:", esp)
+
+	proxy := smpt.ProxySetup{
+		Enable:   false,
+		Address:  "212.116.243.131:12323",
+		Username: "14a8e1626bbd4",
+		Password: "95f42a5e9c",
+	}
+
+	verified, err := smpt.VerifyEmailAddress(email, "microsoft.com", "steve.balmer@microsoft.com", proxy)
+	fmt.Println("Verified:", verified)
+	fmt.Println(err)
 }
