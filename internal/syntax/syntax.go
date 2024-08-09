e998674f6aef7fd5daae8aaa7a5fa5962d45727e
alexopenline <alex@openline.ai>
2024-08-09 09:54:35 +0300

    support unicode in email username

--- internal/syntax/syntax.go
+++ internal/syntax/syntax.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	usernameRegex = regexp.MustCompile(`^[a-zA-Z0-9.!#$%&'+-/=?^_` + "`" + `{|}~]+$`)
+	usernameRegex = regexp.MustCompile(`^[\p{L}\p{N}.!#$%&'+-/=?^_` + "`" + `{|}~]+$`)
 	domainRegex   = regexp.MustCompile(`^[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9](?:\.[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9])*$`)
 )
 
