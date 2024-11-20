96e3ec9de94cfff4533fbe6bf9f4ca1635867dc6
mattbr0wn <matt@openline.ai>
2024-11-20 16:13:06 +0000

    remove domaingen

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/customeros/mailsherpa/cli"
 	"github.com/customeros/mailsherpa/domaincheck"
-	"github.com/customeros/mailsherpa/domaingen"
 	"github.com/customeros/mailsherpa/emailparser"
 )
 
@@ -15,9 +14,6 @@ func main() {
 	args := flag.Args()
 
 	switch args[0] {
-	case "avail":
-		_, avail := domaingen.IsDomainAvailable(args[1])
-		fmt.Println("Domain Available:", avail)
 	case "domain":
 		if len(args) != 2 {
 			fmt.Println("Usage: mailsherpa domain <domain>")
@@ -30,8 +26,6 @@ func main() {
 			return
 		}
 		cli.VerifySyntax(args[1], true)
-	case "rec":
-		fmt.Println(domaingen.RecommendOutboundDomains(args[1], 20))
 	case "redirect":
 		fmt.Println(domaincheck.PrimaryDomainCheck(args[1]))
 	case "parse":
